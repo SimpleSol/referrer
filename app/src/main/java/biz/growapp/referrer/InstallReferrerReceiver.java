6ddea4824353381360946d2fb7b52654abc85816
SimpleSol <gworkmaill@gmail.com>
2016-10-07 12:08:51 +0300

    write url to file

--- app/src/main/java/biz/growapp/referrer/InstallReferrerReceiver.java
+++ app/src/main/java/biz/growapp/referrer/InstallReferrerReceiver.java
@@ -3,7 +3,11 @@ package biz.growapp.referrer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class InstallReferrerReceiver extends BroadcastReceiver {
 
@@ -13,6 +17,14 @@ public class InstallReferrerReceiver extends BroadcastReceiver {
         Toast.makeText(context, url, Toast.LENGTH_LONG).show();
         Prefs.edit().putString(Prefs.PREFS, url).apply();
         App.rxBus.send(url);
+        File file;
+        FileOutputStream outputStream;
+        try {
+            file = new File(Environment.getExternalStorageDirectory(), "qqqqq");
+            outputStream = new FileOutputStream(file);
+            outputStream.write(url.getBytes());
+            outputStream.close();
+        } catch (Exception ignored) {}
     }
 
 }
