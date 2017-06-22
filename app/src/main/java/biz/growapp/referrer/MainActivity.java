099dafebc5a03dd15cc58eb41e4aa5c765011447
SimpleSol <gworkmaill@gmail.com>
2017-06-22 12:37:03 +0300

    do not remember, just commit changes

--- app/src/main/java/biz/growapp/referrer/MainActivity.java
+++ app/src/main/java/biz/growapp/referrer/MainActivity.java
@@ -1,19 +1,21 @@
 package biz.growapp.referrer;
 
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import rx.Observer;
+
+import static biz.growapp.referrer.R.id.btnPrefs;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView tvUrl;
     private TextView tvPrefs;
-    private Button btnPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvUrl = (TextView) findViewById(R.id.text);
         tvPrefs = (TextView) findViewById(R.id.tvPrefs);
-        btnPrefs = (Button) findViewById(R.id.btnPrefs);
-        btnPrefs.setOnClickListener(this);
+        findViewById(R.id.btnPrefs).setOnClickListener(this);
+        findViewById(R.id.btnInstalledFrom).setOnClickListener(this);
 
         App.rxBus.observeEvents().subscribe(new Observer() {
             @Override
@@ -46,10 +48,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnPrefs) {
+        if (v.getId() == btnPrefs) {
             SharedPreferences preferences = getSharedPreferences(InstallReferrerReceiver.MY_PREFS, MODE_PRIVATE);
             String referrer = preferences.getString(InstallReferrerReceiver.REFERRER_KEY, "not today");
             tvPrefs.setText(referrer);
+        } else if (v.getId() == R.id.btnInstalledFrom) {
+            Toast.makeText(this, getInstaller(), Toast.LENGTH_LONG).show();
         }
+    }
+
+    private String getInstaller() {
+        PackageManager pm = getPackageManager();
+        return pm.getInstallerPackageName(getPackageName());
     }
 }
